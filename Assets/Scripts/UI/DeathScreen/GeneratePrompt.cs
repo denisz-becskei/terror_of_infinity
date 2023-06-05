c9f5161e7b02139b02742e9f9f3f376912d228f8
denisz-becskei <dbecskei1@outlook.com>
2023-06-05 22:32:42 +0200

    Bug Fixes regarding Death Screen prompts

--- Assets/Scripts/UI/DeathScreen/GeneratePrompt.cs
+++ Assets/Scripts/UI/DeathScreen/GeneratePrompt.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 public class GeneratePrompt : MonoBehaviour
 {
-    private void Start()
+    private void Awake()
     {
         var promptList = Resources.Load("DeathPrompts").ToString().Split('\n');
         GetComponent<TMP_Text>().text = promptList[Random.Range(0, promptList.Length)].Trim();
